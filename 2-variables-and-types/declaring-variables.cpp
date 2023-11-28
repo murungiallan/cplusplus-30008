19450211c95ca7863307190293af176e9e41cc55
Allan <allancheboiw0@gmail.com>
2023-11-28 11:16:11 +0800

    Variables and types

---
+++ 2-variables-and-types/declaring-variables.cpp
@@ -0,0 +1,22 @@
+// operating with variables
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare variables
+    int a, b;
+    int result;
+
+    // process
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+
+    // print out result
+    cout << result;
+
+    // terminate program
+    return 0;
+}
